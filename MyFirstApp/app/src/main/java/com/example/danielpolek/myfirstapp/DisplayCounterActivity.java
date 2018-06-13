a9569ca172dada033968e75c1529cc5cea055b0e
Polek-Pawlak, Daniel <daniel.polek-pawlak@firma.interia.pl>
2018-06-13 17:59:56 +0200

    jeszcze nie działa

--- MyFirstApp/app/src/main/java/com/example/danielpolek/myfirstapp/DisplayCounterActivity.java
+++ MyFirstApp/app/src/main/java/com/example/danielpolek/myfirstapp/DisplayCounterActivity.java
@@ -2,6 +2,7 @@ package com.example.danielpolek.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisplayCounterActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class DisplayCounterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_content);
+    }
+
+    public void addOne(View view) {
+    }
+
+    public void subtractOne(View view) {
+    }
+
+    public void resetCounter(View view) {
+    }
+
+    public void setValueCounter(View view) {
     }
 }
